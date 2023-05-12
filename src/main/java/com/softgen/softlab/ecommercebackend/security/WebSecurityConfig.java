aad3bc8a1e47ac8424e7d391d0bbda059e8da82a
Levan Kiknavelidze <levank.1979@gmail.com>
2023-05-12 22:53:34 +0400

    2

--- src/main/java/com/softgen/softlab/ecommercebackend/security/WebSecurityConfig.java
+++ src/main/java/com/softgen/softlab/ecommercebackend/security/WebSecurityConfig.java
@@ -1,17 +1,25 @@
 package com.softgen.softlab.ecommercebackend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JWTRequestFilter jwtRequestFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
       http.csrf().disable().cors().disable();
-      http.authorizeHttpRequests().anyRequest().permitAll();
+      http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+      http.authorizeHttpRequests()
+              .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+              .anyRequest().authenticated();
       return http.build();
     }
 
